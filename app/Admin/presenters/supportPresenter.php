2df4ba1f232badfdb68aedf04a422f3cde499684
janikd <contact.janik@seznam.cz>
2015-02-16 20:23:00 +0100

    design

---
+++ app/Admin/presenters/supportPresenter.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\AdminModule\Presenters;
+
+use Nette,
+    App\AdminModule\Model,
+    Nette\Database;
+
+class supportPresenter extends BasePresenter
+{
+    protected function startup()
+    {
+        parent::startup();
+    }
+}
+
