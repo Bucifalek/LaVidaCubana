12608e7787ce630145b75c70ab264f3acc8e7dba
David Janík <contact.janik@seznam.cz>
2015-04-07 19:58:53 +0200

    Týmy a databejz

--- app/AdminModule/model/TeamsManager.php
+++ app/AdminModule/model/TeamsManager.php
@@ -11,15 +11,39 @@ use Nette;
 class TeamsManager extends Nette\Object
 {
 
-	private $database;
+    private $database;
 
-	function __construct(Nette\Database\Context $database)
-	{
-		$this->database = $database;
-	}
+    function __construct(Nette\Database\Context $database)
+    {
+        $this->database = $database;
+    }
 
-	public function getAll()
-	{
-		return $this->database->table(DatabaseStructure::BOWLING_TEAMS)->fetchAll();
-	}
+    public function getAll()
+    {
+        return $this->database->table(DatabaseStructure::BOWLING_TEAMS)->order('score DESC')->fetchAll();
+    }
+
+    public function total()
+    {
+        return $this->database->table(DatabaseStructure::BOWLING_TEAMS)->count();
+    }
+
+    public function getPage($limit, $offset)
+    {
+        $teamsFinal = [];
+        $teams = $this->database->table(DatabaseStructure::BOWLING_TEAMS)->limit($limit, $offset)->order('score DESC')->fetchAll();
+        foreach ($teams as $team) {
+            $teamsFinal[] = [
+                'name' => $team->name,
+                'score' => $team->score,
+                'score_avg' => $team->score_avg,
+                'index' => $team->index,
+                'matches' => $team->matches,
+                'helpers' => $team->helpers,
+                'points' => $team->points,
+            ];
+        }
+
+        return $teamsFinal;
+    }
 }
