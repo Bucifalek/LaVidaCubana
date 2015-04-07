8c8c36cfdf126c553aa27c79d7b940cf319071f2
Jan Kotrba <jan.kotrbaa@gmail.com>
2015-04-07 19:27:57 +0200

    Data support

--- app/AdminModule/model/DatabaseStructure.php
+++ app/AdminModule/model/DatabaseStructure.php
@@ -19,29 +19,34 @@ class DatabaseStructure extends Nette\Object
 	 * Bowling players table
 	 */
 	const BOWLING_PLAYERS = "bowling_players";
+
 	/**
 	 * Bowling teams table
 	 */
 	const BOWLING_TEAMS = "bowling_teams";
+
 	/**
 	 * Web branches table
 	 */
 	const BRANCHES = "branches";
+
 	/**
 	 * Main page news table
 	 */
 	const MAIN_NEWS = "main_news";
+
 	/**
 	 * Pages table
 	 */
 	const PAGES = "pages";
+
 	/**
 	 * Pages content table
 	 */
 	const PAGES_CONTENT = "pages_content";
+
 	/**
 	 * Users table
 	 */
 	const USERS = "users";
-
 }
