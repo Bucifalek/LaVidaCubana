d77046032b83c4c98d26b8fa8514d84d7fabbe56
Jan Kotrba <jan.kotrbaa@gmail.com>
2015-04-14 23:17:44 +0200

    Revert "Admin Designs v1.4"

--- AdminDesigns/vendor/plugins/circles/circles.js
+++ AdminDesigns/vendor/plugins/circles/circles.js
@@ -326,36 +326,4 @@
   Circles.create = function(options) {
     return new Circles(options);
   };
-
-
-  // Helper function that adds responsive ability to chosen text elements
-  $.fn.fitText = function( kompressor, options ) {
-
-      // Setup options
-      var compressor = kompressor || 1,
-          settings = $.extend({
-            'minFontSize' : Number.NEGATIVE_INFINITY,
-            'maxFontSize' : Number.POSITIVE_INFINITY
-          }, options);
-
-      return this.each(function(){
-
-        // Store the object
-        var $this = $(this);
-
-        // Resizer() resizes items based on the object width divided by the compressor * 10
-        var resizer = function () {
-          $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-        };
-
-        // Call once to set.
-        resizer();
-
-        // Call on resize. Opera debounces their resize by default.
-        $(window).on('resize.fittext orientationchange.fittext', resizer);
-
-      });
-  };
-
-
 })();
