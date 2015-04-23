05ddf9efdf0eb0d81e6a12323b7fdb7846d7c308
Jan Kotrba <jan.kotrbaa@gmail.com>
2015-04-23 19:26:42 +0200

    updates

--- app/AdminModule/presenters/SignPresenter.php
+++ app/AdminModule/presenters/SignPresenter.php
@@ -22,6 +22,30 @@ final class SignPresenter extends BasePresenter
 	/** @persistent */
 	public $backlink;
 
+	/**
+	 * @var Model\PasswordRecoveryTokens
+	 */
+	private $recoveryTokens;
+
+	private $userManager;
+
+	/**
+	 * @param Model\UserManager $userManager
+	 * @param Nette\Database\Context $database
+	 * @param Model\BranchManager $branchManager
+	 * @param Model\PasswordRecoveryTokens $recoveryTokens
+	 */
+	function __construct(Model\UserManager $userManager, Nette\Database\Context $database, Model\BranchManager $branchManager, Model\PasswordRecoveryTokens $recoveryTokens)
+	{
+		parent::__construct($userManager, $database, $branchManager);
+		$this->recoveryTokens = $recoveryTokens;
+		$this->userManager = $userManager;
+	}
+
+
+	/**
+	 *
+	 */
 	public function renderIn()
 	{
 		if ($this->getUser()->isLoggedIn()) {
@@ -29,20 +53,78 @@ final class SignPresenter extends BasePresenter
 		}
 	}
 
-	/** Not working */
-	public function createComponentForgotDetailsForm() {
+	/**
+	 * @param $token
+	 */
+	public function renderRecovery($token)
+	{
+		if (!$token) {
+			$this->flashMessage('Neplatný pokus o obnovení hesla!', FLASH_FAILED);
+			$this->redirect('Sign:in');
+		}
+		try {
+			$this->recoveryTokens->isValid($token);
+		} catch (\Exception $e) {
+			$this->flashMessage($e->getMessage(), FLASH_FAILED);
+			$this->redirect('Sign:forgot');
+		}
+
+
+		$this->template->newPassowrd = Nette\Utils\Random::generate(30, '0-9a-zA-Z');
+	}
+
+
+	/**
+	 * @return UI\Form
+	 */
+	public function createComponentForgotDetailsForm()
+	{
 		$form = new UI\Form();
+		$form->addProtection();
 		$form->addText('email');
 		$form->addSubmit('restore');
-		$form->onSubmit[] = [$this, 'startRestore'];
+		$form->onSuccess[] = [$this, 'startRestore'];
 
 		return $form;
 	}
 
-	/** Not working */
-	public function startRestore(UI\Form $form) {
+
+	public function startRestore(UI\Form $form)
+	{
 		$values = $form->getValues();
-		Debugger::barDump($values);
+		if(!Nette\Utils\Validators::isEmail($values['email'])) {
+			$this->flashMessage('Neplatná emailová adresa.', FLASH_FAILED);
+			$this->redirect('Sign:forgot');
+		}
+		if(!$this->userManager->isAsociated($values['email'])) {
+			$this->flashMessage('Tento email nepatří k žádnému účtu.', FLASH_FAILED);
+			$this->redirect('Sign:forgot');
+		}
+
+		$token = $this->recoveryTokens->generate();
+
+		$this->recoveryTokens->save();
+		$this->flashMessage('Email s odkazem pro obnovení hesla odeslán.', FLASH_SUCCESS);
+
+
+		//try {
+			$myMailer = new Model\MyMailer;
+			$myMailer->setHtmlBody(
+				__DIR__ . '/../templates/EmailTemplates/restorePassword.latte',
+				[
+					'link' => $this->link('Sign:recovery', ['token' => $token]),
+				]
+			)
+				->addTo($values['email'])
+				->setFrom("upozorneni@kotyslab.cz")
+				->setSubject("Obnovení hesla");
+			$myMailer->sendEmail();
+		/*} catch (\Exception $e) {
+			Debugger::barDump($e);
+			$this->flashMessage($e->getMessage(), FLASH_WARNING);
+		}*/
+
+		$this->redirect('Sign:in');
 	}
 
 	/**
